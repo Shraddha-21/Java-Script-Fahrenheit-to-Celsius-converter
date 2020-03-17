628d9d132564c5da2574d0ef9717dd7ede1cfdea
Shraddha-21 <60236427+Shraddha-21@users.noreply.github.com>
2020-03-16 21:10:02 -0500

    Add files via upload

---
+++ Temp.js
@@ -0,0 +1,6 @@
+var $ = function(id) {
+    return document.getElementById(id);
+};
+
+To convert Fahrenheit to Celsius, first subtract 32 from the Fahrenheit
+temperature. Then, multiply that result by 5/9.
